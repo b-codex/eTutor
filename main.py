7c04892a31912751696a36c655f287864990fe1f
theTruth <61096394+MyoniM@users.noreply.github.com>
2021-05-31 01:12:43 +0300

    Update main.py

--- main.py
+++ main.py
@@ -10,15 +10,7 @@ from operator import itemgetter
 from flask_mobility import Mobility
 
 
-firebaseConfig = {
-    "apiKey": "AIzaSyDnN_lXnJRwcZrO3SBIbINjR20iJ5XhJyQ",
-    "authDomain": "temaribet-af8a0.firebaseapp.com",
-    "databaseURL": "https://temaribet-af8a0-default-rtdb.firebaseio.com",
-    "projectId": "temaribet-af8a0",
-    "storageBucket": "temaribet-af8a0.appspot.com",
-    "messagingSenderId": "361477504348",
-    "appId": "1:361477504348:web:bb1f559f4687130e691599"
-}
+
 
 firebase = pyrebase.initialize_app(firebaseConfig)
 
